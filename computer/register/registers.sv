4c8c706a4844f787282238291e4fb03f9fa30dab
progrugger <schadenfreude.hedonism@gmail.com>
2017-10-02 23:41:22 +0900

    register

--- computer/register/registers.sv
+++ computer/register/registers.sv
@@ -1,18 +1,19 @@
 `timescale 1ns / 1000ps
 `default_nettype none
 
-module gen_registers(
-   input wire [31:0] inreg [0:15],
-   input wire enable [0:15],
+module registers(
+   input wire [31:0] inreg [0:31],
+   input wire [31:0] enable,
    input wire clk,
-   output wire [31:0] outreg [0:15]);
+   output wire [31:0] outreg [0:31]);
 
    // r0 : zero register
    assign outreg[0] = {32'b0}
 
    // r1 - r15 : general registers
+   // f0 - f15 (r16 - r31) : float registers
    genvar i;
-   for (i=1; i<16; i=i+1) begin
+   for (i=1; i<31; i=i+1) begin
       register i_r(.inp(inreg[i]), .clk(clk), .enable(enable[i]), .outp(outreg[i]));
    end
 
