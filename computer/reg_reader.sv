08310baa222bcb0a70a609ce2508051716600031
progrugger <schadenfreude.hedonism@gmail.com>
2017-10-09 01:10:45 +0900

    register

---
+++ computer/reg_reader.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 100ps
+`default_nettype none
+
+module reg_reader (
+   input wire r_gfflag, // 0:general 1:float
+   input wire [3:0] r_num,
+   output wire [31:0] r_data,
+
+   input wire [31:0] regsout [0:31];
+
+   input wire clk)
+
+   wire [31:0] regsout [0:31];
+
+   wire [4:0] rnum = {r_gfflag, r_num};
+   
+   assign r_data = regsout[rnum];
+
+endmodule
+
+`default_nettype wire
---
+++ computer/reg_writer.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 100ps
+`default_nettype none
+
+module reg_writer (
+   input wire r_gfflag, // 0:general 1:float
+   input wire [3:0] r_num,
+   input wire [31:0] r_data,
+   input wire enable, // 0:disable 1:enable
+
+   output wire [31:0] regsin [0:31],
+   output wire [31:0] enables,
+
+   input wire clk);
+
+
+   wire [4:0] rnum = {r_gfflag, r_num};
+
+   wire [31:0] enables = {31'b0, 1'b1} << rnum;
+
+   assign regsin[rednum] = r_data;
+
+endmodule
+
+`default_nettype wire
