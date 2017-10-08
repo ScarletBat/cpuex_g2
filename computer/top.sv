77df61a2a96be7ba111c4f9ef20aa89f31044a3c
progrugger <schadenfreude.hedonism@gmail.com>
2017-10-09 01:10:56 +0900

    top

--- computer/top.sv
+++ computer/top.sv
@@ -3,6 +3,15 @@
 
 module top();
 
+    wire [31:0] regsin [0:31];
+    wire [31:0] regsout [0:31];
+    wire [31:0] regenable;
+
+    reg_writer reg_wrer(.r_gfflag(), .r_num(), .r_data(), .enable(), .regsin(regsin), .enables(regenable), .clk(clk));
+    registers regs(.inreg(regsin), .enable(regenable), .clk(clk), .outreg(regsout));
+    reg_reader reg_rder1(.r_gfflag(), .r_num(), .r_data(), .regsout(regsout), .clk(clk));
+    reg_reader reg_rder2(.r_gfflag(), .r_num(), .r_data(), .regsout(regsout), .clk(clk));
+
 endmodule
 
 `default_nettype wire
