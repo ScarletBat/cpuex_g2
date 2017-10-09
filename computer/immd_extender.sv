18edc3862cc2b47eddd463760fcd52a4a906fcb6
progrugger <schadenfreude.hedonism@gmail.com>
2017-10-10 01:58:06 +0900

    immd_extender

---
+++ computer/immd_extender.sv
@@ -0,0 +1,13 @@
+`timescale 1ns / 100ps
+`default_nettype none
+
+module immd_extender(
+    input wire [15:0] immd,
+    input wire zors,
+    output wire eximmd);
+
+    assign eximmd = zors == 1'b0 ? {16'b0, immd} : {16(immd[15]), immd};
+
+endmodule 
+
+`default_nettype wire
