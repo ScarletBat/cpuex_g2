47adcdd4df41e07391bd13d2c1154b0273029ef6
progrugger <progrugger@example.com>
2017-10-10 14:36:40 +0900

    top

--- computer/top.sv
+++ computer/top.sv
@@ -3,6 +3,9 @@
 
 module top();
 
+    wire [31:0] cor_pc;
+    wire [31:0] nxt_pc;
+
     wire [31:0] inst;
     wire [5:0] opecode;
     wire [4:0] rd;
@@ -12,7 +15,10 @@ module top();
     wire [5:0] funct;
     wire [15:0] immd;
     wire [25:0] addr;
-    wire [1:0] opetype;
+
+    wire [5:0] alu_funct;
+    wire in_gof;
+    wire out_gof;
 
     wire [31:0] regsin [0:31];
     wire [31:0] regsout [0:31];
@@ -25,16 +31,19 @@ module top();
     wire [31:0] alu_data;
     wire immflag;
 
-    inst_decoder inst_decoder(.inst(inst), .opecode(opecode), .rd(rd), .rs(rs), .rt(rt), .shamt(shamt), .funct(funct), .immd(immd), .addr(addr), .opetype(opetype));
+    register pc(.inp(nxt_pc), .clk(clk), .enable(), .outp(cor_pc));
+    register lr(.inp(cor_pc), .clk(clk), .enable(), .outp());
 
-reg_writer reg_writer(.r_gfflag(), .r_num(rd), .r_data(rd_data), .enable(), .regsin(regsin), .enables(regenable), .clk(clk));
+    inst_decoder inst_decoder(.inst(inst), .opecode(opecode), .rd(rd), .rs(rs), .rt(rt), .shamt(shamt), .funct(funct), .immd(immd), .addr(addr));
+
+    reg_writer reg_writer(.r_gfflag(out_gof), .r_num(rd), .r_data(rd_data), .enable(), .regsin(regsin), .enables(regenable), .clk(clk));
     registers regs(.inreg(regsin), .enable(regenable), .clk(clk), .outreg(regsout));
-    reg_reader reg_reader1(.r_gfflag(), .r_num(rs), .r_data(rs_data), .regsout(regsout), .clk(clk));
-    reg_reader reg_reader2(.r_gfflag(), .r_num(rt), .r_data(rt_data), .regsout(regsout), .clk(clk));
+    reg_reader reg_reader1(.r_gfflag(in_gof), .r_num(rs), .r_data(rs_data), .regsout(regsout), .clk(clk));
+    reg_reader reg_reader2(.r_gfflag(in_gof), .r_num(rt), .r_data(rt_data), .regsout(regsout), .clk(clk));
 
     immd_extender(.immd(immd), .zors(), .eximmd(eximmd));
     data_selector data_selector1(.data0(rt_data), .data1(eximmd), .choice(), .odata(alu_data));
-    alu alu(.rs(rs_data), .rt(), .funct(funct), .shamt(shamt), .rd());
+    alu alu(.rs(rs_data), .rt(), .funct(alu_funct), .shamt(shamt), .rd());
 
     pc_incrementer pc_incrementer(.cpc(), .npc());
 
